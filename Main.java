9c6347268e74068bdeb1bdec0486a3932576d1d6
LEONEL RANGEL COQUITO <135623583+LeonelRangelCoquito@users.noreply.github.com>
2026-07-13 16:08:32 +0000

    Criação da classe personagem e classe main

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+// Criar a classe main que é a classe que executa o programa java e é onde instaciamos os objetos
+public class Main{
+    public static void main(String[] args){
+        // Instanciando os objetos "Personagem"
+        Personagem p1 = new Personagem("Hanzo","Guerreiro",1,100,20.5);
+        Personagem p2 = new Personagem("Zhask","Mago",1,100,50.5);
 
-        .
-        .
-        .
+        // Chamando o método exibirStatus()
+        p1.exibirStatus();
+        p2.exibirStatus();
+
+        // teste de modificação de atributos do obejto p1
+        //p1.classe = "Assassino";
+
+        // Exibir p1 apos modificação
+        //p1.exibirStatus();
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,33 @@
+// Criação da classe personagem
 public class Personagem {
+
+    // Criação das variaveis de classe
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
-
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    
+    // Construtor da classe Personagem
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase){
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
-
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+    
+    /*
+    Metodo de classe que exibe as informações do personagem. Saída :
+    
+    Nome : ...
+    Clase : ...
+    Nivel : ...
+    Pontos de vida : ...
+    Poder base : ...
+    
+    */
+    public void exibirStatus(){
+        System.out.printf("\nNome : %s\nClasse : %s\nNível : %d\nPontos de vida : %d\nPoder base : %f\n",nome,classe,nivel,pontosDeVida,poderBase);
     }
 }
-
